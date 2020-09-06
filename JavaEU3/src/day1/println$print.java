093d8b82abe57e59e6989d6095bee2d94e7c7daa
Alinur Yaser <alinuer.jasean@gmail.com>
2020-09-06 22:54:20 +0200

    second commit

---
+++ JavaEU3/src/day1/println$print.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class println$print {
+
+	public static void main(String[] args) {
+
+	System.out.println("with new line");	
+	
+	System.out.print("printing without new line");	
+		
+		
+		
+	}
+
+}
